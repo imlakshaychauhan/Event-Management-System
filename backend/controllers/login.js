0e08ef3563816d7d4e4c96ebee20ff6cf0ad4262
Lakshay Chauhan <imlakshaychauhan@gmail.com>
2024-03-14 20:23:00 +0530

    bcrypt to bcryptjs done

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,5 +1,5 @@
 const loginRouter = require("express").Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const config = require("../utils/config");
